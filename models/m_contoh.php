a7c90db7b03f5acd26c3feab33426f2d9d3632b0
proncesslucu <salsabilakeisha041@gmail.com>
2024-12-16 08:45:25 +0700

    menambahkan fungsi

--- models/m_contoh.php
+++ models/m_contoh.php
@@ -1,3 +1,23 @@
 <?php
 
-class m_contoh {}
+class m_contoh 
+{
+
+    // bikin 3 fungsi tampil, edit, hapus 
+
+    public function tampil()
+    {
+        //fungsi untuk menanpilkan data dari database
+    }
+    function edit()
+    {
+        //fungsi untuk mengedit data dari database
+    }
+    function hapus()
+    {
+        //fungsi untuk menghapus data dari database
+    }
+
+}
+
+?>
